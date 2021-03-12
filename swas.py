142330ba30e18d475977ceb12692d4deb8a7d858
Swas.py <61446939+CodeWithSwastik@users.noreply.github.com>
2021-03-12 19:13:44 +0530

    Create swas.py

---
+++ swas.py
@@ -0,0 +1,14 @@
+from sys import argv
+from executor import execute, shell
+
+def main():
+    
+    if len(argv) <= 1:
+        shell()
+    else:
+        fp = argv[1]
+        execute(fp)
+
+if __name__ == '__main__':
+    names = {}
+    main()
