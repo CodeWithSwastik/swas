dcf95b60931d87951583e8666214a00dcece2f38
FalseDev <59121948+FalseDev@users.noreply.github.com>
2021-03-14 15:35:25 +0530

    Fix CORS

--- api/run.py
+++ api/run.py
@@ -33,6 +33,7 @@ class handler(BaseHTTPRequestHandler):
 
         self.send_response(200)
         self.send_header("Content-type", "application/json")
+        self.send_header("Access-Control-Allow-Origin", "*")
         self.end_headers()
         self.wfile.write(json.dumps(data).encode())
 
